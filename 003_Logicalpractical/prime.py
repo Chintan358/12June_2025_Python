b67a052b751e3e8a7ab4c6c3da1383cdc82743cf
chintan <chintan@gmail.com>
2025-06-28 15:23:08 +0530

    all

--- 003_Logicalpractical/prime.py
+++ 003_Logicalpractical/prime.py
@@ -1,10 +1,12 @@
-number = 13
+number = 15
 flag = 0
+
 for i in range(2,number):
     if number%i==0:
         flag = 1
         break
-if flag==0:
-    print("prime")
+
+if flag ==0:
+    print("Prime")
 else:
-    print("not prime")
+    print("Not prime")
