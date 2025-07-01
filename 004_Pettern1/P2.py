28b79e19600e78afb48963234e5fac1afb76d420
chintan <chintan@gmail.com>
2025-07-01 08:41:52 +0530

    pettern

---
+++ 004_Pettern1/P2.py
@@ -0,0 +1,31 @@
+
+
+# *
+# **
+# ***
+# ****
+# *****
+
+
+# *****
+# ****
+# ***
+# **
+# *
+
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+# starts=1
+# for i in range(5):
+#     for j in range(starts):
+#         print("*",end="")
+#     print()
+#     starts+=1
+
+
+# for i in range(1,6):
+#     print("*"*i)
