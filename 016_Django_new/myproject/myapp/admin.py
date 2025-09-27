8cefb33b9923c15aaf1268db8e28f66787cf1152
chintan <chintan@gmail.com>
2025-09-27 09:01:00 +0530

    all

--- 016_Django_new/myproject/myapp/admin.py
+++ 016_Django_new/myproject/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from myapp.models import *
 # Register your models here.
+
+class EmpModel(admin.ModelAdmin):
+    list_display=['id','name','email','dept','jdata']
+
+admin.site.register(Student)
+admin.site.register(Employee,EmpModel)
+admin.site.register(Product)
--- 016_Django_new/myproject/myapp/models.py
+++ 016_Django_new/myproject/myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    age = models.IntegerField()
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    dept = models.CharField(max_length=20)
+    jdata = models.DateField()
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+    qty = models.IntegerField()
+    
+
