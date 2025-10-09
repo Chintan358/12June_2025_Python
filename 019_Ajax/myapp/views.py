6e4369c36e814bf80d583c0bfdb412040e00658e
chintan <chintan@gmail.com>
2025-10-09 08:45:58 +0530

    ajax

--- 019_Ajax/myapp/views.py
+++ 019_Ajax/myapp/views.py
@@ -10,3 +10,18 @@ def search(request):
     # products = Product.objects.filter(name=data)
     products = Product.objects.filter(name__startswith=data)
     return JsonResponse({"data":list(products.values())})
+
+
+def countries(request):
+    allcountries = Country.objects.all()
+    return JsonResponse({"data":list(allcountries.values())})
+
+def states(request):
+    cid = request.GET['cid']
+    allStates = State.objects.filter(country_id=cid)
+    return JsonResponse({"data":list(allStates.values())})
+
+def cities(request):
+    sid = request.GET['sid']
+    allCities = City.objects.filter(state_id=sid)
+    return JsonResponse({"data":list(allCities.values())})
