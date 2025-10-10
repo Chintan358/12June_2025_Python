e1c557633e082760b8ddaa95650f466b2b09501e
chintan <chintan@gmail.com>
2025-10-10 09:02:38 +0530

    all

---
+++ 020_AJAXCrud/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from myapp.models import *
+# Register your models here.
+
+class StudentModel(admin.ModelAdmin):
+    list_display=['id','name','email','phone','age']
+
+admin.site.register(Student,StudentModel)
---
+++ 020_AJAXCrud/myapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,HttpResponse
+from myapp.models import Student
+from django.http import JsonResponse
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def register(request):
+    if request.method=='POST':
+        data = request.POST
+        name = data.get('name')
+        email = data.get('email')
+        phone = data.get('phone')
+        age = data.get('age')
+
+        Student.objects.create(name=name,email=email,phone=phone,age=age)
+        return HttpResponse("Registration successfull Done !!!")
+    
+def display(request):
+    allStudents = Student.objects.all()
+    return JsonResponse({"data":list(allStudents.values())})
