1eb861ba444dbd8460904751f20d41d6625a29c5
chintan <chintan@gmail.com>
2025-11-08 09:06:32 +0530

    all

---
+++ eshop/myapp/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from myapp.models import *
+# Register your models here.
+
+class CategoryModel(admin.ModelAdmin):
+    list_display=['id','name','image']
+
+class ProductModel(admin.ModelAdmin):
+    list_display=['id','name','price','qty','image','category']
+
+admin.site.register(Category,CategoryModel)
+admin.site.register(Product,ProductModel)
+admin.site.register(Cart)
+admin.site.register(Address)
+admin.site.register(UserOrder)
+admin.site.register(OrderDetails)
+
---
+++ eshop/myapp/urls.py
@@ -0,0 +1,34 @@
+
+
+from django.urls import path
+from myapp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path("",index,name="index"),
+    path("accounts",accounts,name="accounts"),
+    path("cart",cart,name="cart"),
+    path("checkout",checkout,name="checkout"),
+    path("compare",compare,name="compare"),
+    path("details",details,name="details"),
+    path("login-register",login_register,name="login-register"),
+    path("shop",shop,name="shop"),
+    path("wishlist",wishlist,name="wishlist"),
+    path("allcategories",allcategories,name="allcategories"),
+    path("allproducts",allproducts,name="allproducts"),
+
+    path("register",register,name="register"),
+    path("login",user_login,name="login"),
+    path("user-logout",user_logout,name="user-logout"),
+
+    path('addtocart',add_to_cart,name="addtocart"),
+    path('deletecart',deletecart,name='deletecart'),
+    path('changeqty',changeqty,name='changeqty'),
+
+    path('payment',payment,name='payment'),
+    path("addaddress",addaddress,name="addaddress"),
+    path("makeorder",makeorder,name="makeorder")
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
