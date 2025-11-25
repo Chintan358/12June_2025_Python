c34b5055f427d0045976c50115ad8e2efe6c8b3b
chintan <chintan@gmail.com>
2025-11-25 09:07:44 +0530

    auth

--- drf/myapp/views.py
+++ drf/myapp/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render,HttpResponse
-from rest_framework.decorators import api_view,APIView
+from rest_framework.decorators import api_view,APIView,permission_classes
 from myapp.models import *
 from rest_framework.response import Response
 from myapp.serializer import *
+from rest_framework.permissions import IsAuthenticated
+from myapp.permissions import *
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_users(request):
     return HttpResponse("GET API CALLING")
 
+
 @api_view(['POST'])
+@permission_classes([isAdminUserOnly])
 def post_users(request):
     return HttpResponse("POST API CALLING")
 
@@ -24,6 +30,8 @@ def test(request):
     return HttpResponse("")
 
 class StudentView(APIView):
+
+
 
     def get(self,request):
         allstudents = Student.objects.all()
